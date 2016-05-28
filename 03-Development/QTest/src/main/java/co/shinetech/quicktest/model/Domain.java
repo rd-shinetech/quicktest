aec0f7c0cd90ed9a94a0bedfd39e8c244cb9adca
rd-shinetech <rodrigo@shinetech.co>
2016-05-28 08:12:58 +0100

    Initial artifact.

---
+++ 03-Development/QTest/src/main/java/co/shinetech/quicktest/model/Domain.java
@@ -0,0 +1,13 @@
+/**
+ * Domain.java
+ */
+package co.shinetech.quicktest.model;
+
+/**
+ * Interface for all DTO/Model classes
+ * @author rodrigo
+ * @since 2016/05
+ */
+public interface Domain {
+
+}
