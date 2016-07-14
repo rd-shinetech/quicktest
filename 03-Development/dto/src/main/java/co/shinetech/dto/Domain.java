5494df17ebd832b5b678f029e144f06beb344eb7
rdinis <rdinis@CNU408DL7L.co-int.net>
2016-07-14 10:24:05 +0100

    Module dto creation.

---
+++ 03-Development/dto/src/main/java/co/shinetech/dto/Domain.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package co.shinetech.dto;
+
+/**
+ * @author rdinis
+ *
+ */
+public class Domain {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}	
+}
