55837a37dbea40b95f76f5d30282d8ae927b5897
RobinDil <robinvassantlal@hotmail.com>
2016-07-14 22:18:16 +0100

    user update

--- 03-Development/dto/src/main/java/co/shinetech/dto/User.java
+++ 03-Development/dto/src/main/java/co/shinetech/dto/User.java
@@ -23,27 +23,22 @@ public class User implements Domain {
 		return login;
 	}
 
-	
 	public void setLogin(String login) {
 		this.login = login;
 	}
 
-	
 	public char[] getPassword() {
 		return password;
 	}
 
-	
 	public void setPassword(char[] password) {
 		this.password = password;
 	}
 
-	
 	public Profile getProfile() {
 		return profile;
 	}
 
-	
 	public void setProfile(Profile profile) {
 		this.profile = profile;
 	}
