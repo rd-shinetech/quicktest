9446875a14c89e1f8236a30c999f5513ac9b6df0
Gonxaluh <goncalochincho@gmail.com>
2016-07-14 21:30:34 +0100

    "Created Profile class and QuestionType enum."

---
+++ 03-Development/dto/src/main/java/co/shinetech/dto/Profile.java
@@ -0,0 +1,42 @@
+package co.shinetech.dto;
+
+public class Profile implements Domain{
+	private long id;
+	private String name;
+	
+	public Profile(long id, String name){
+		this.id = id;
+		this.name = name;
+	}
+	
+	public long getPk(){
+		return id;
+	}
+	
+	public void setPk(long pk){
+		id = pk;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	@Override
+	public String toString(){
+
+	}
+	
+	@Override
+	public boolean equals(Object other){
+		if(other instanceof Profile){
+			Profile p = (Profile other);
+			return this.id == p.getPk() && this.name.equals(p.getName());
+		}
+		return false;
+	}
+	
+}
