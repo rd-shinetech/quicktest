8ab022de5532c330cd62277df5abd77159170475
ricardopnc <ric_ponc@sapo.pt>
2016-07-18 12:38:44 +0100

    Attributes, access methods, equals, hashCode, to String added.

--- 03-Development/dto/src/main/java/co/shinetech/dto/Result.java
+++ 03-Development/dto/src/main/java/co/shinetech/dto/Result.java
@@ -1,15 +1,80 @@
-/**
- * 
- */
 package co.shinetech.dto;
+
+import java.time.LocalDateTime;
 /**
  * @author Ricardo
  *
  */
-public class Result extends Domain {
+public class Result implements Domain {
 	
+	// Attributes
 	private long id;
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
 	
+	public Result(long id) {
+		this.id = id;
+	}
 	
+	public long getPk() {
+		return id;
+	}	
+	
+	// Access methods
+	public LocalDateTime getStartTime() {
+		return startTime;
+	}
 
+	public void setStartTime(LocalDateTime startTime) {
+		this.startTime = startTime;
+	}
+
+	public LocalDateTime getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(LocalDateTime endTime) {
+		this.endTime = endTime;
+	}
+	
+	// hashCode
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((endTime == null) ? 0 : endTime.hashCode());
+		result = prime * result + ((startTime == null) ? 0 : startTime.hashCode());
+		return result;
+	}
+	
+	// equals
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Result other = (Result) obj;
+		if (endTime == null) {
+			if (other.endTime != null)
+				return false;
+		} else if (!endTime.equals(other.endTime))
+			return false;
+		if (startTime == null) {
+			if (other.startTime != null)
+				return false;
+		} else if (!startTime.equals(other.startTime))
+			return false;
+		return true;
+	}
+	
+	// toString
+	@Override
+	public String toString() {
+		return "Result [startTime=" + startTime + ", endTime=" + endTime + "]";
+	}
+	
+	
 }
--- 03-Development/dto/src/main/java/co/shinetech/dto/ResultItem.java
+++ 03-Development/dto/src/main/java/co/shinetech/dto/ResultItem.java
@@ -1,11 +1,31 @@
-/**
- * 
- */
 package co.shinetech.dto;
 /**
  * @author Ricardo
  *
  */
-public class ResultItem {
+public class ResultItem implements Domain {
+	
+	// Attributes
+	String result;
+	
+	public ResultItem(String result) {
+		this.result = result;
+	}
+	
+	public long getPk() {
+		// TODO Auto-generated method stub
+	return 0;
+	}
+	
+	// Access methods
+	public String getResult() {
+		return result;
+	}
 
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	
+	
 }
