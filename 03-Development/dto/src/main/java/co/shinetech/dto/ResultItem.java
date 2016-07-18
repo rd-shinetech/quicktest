93a00156211409ea17edda286d05dbadd957a997
ricardopnc <ric_ponc@sapo.pt>
2016-07-18 12:40:02 +0100

    Missing file from previous commit

--- 03-Development/dto/src/main/java/co/shinetech/dto/ResultItem.java
+++ 03-Development/dto/src/main/java/co/shinetech/dto/ResultItem.java
@@ -26,6 +26,37 @@ public class ResultItem implements Domain {
 		this.result = result;
 	}
 	
+	// hashCode
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.result == null) ? 0 : this.result.hashCode());
+		return result;
+	}
+
+	// equals
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ResultItem other = (ResultItem) obj;
+		if (result == null) {
+			if (other.result != null)
+				return false;
+		} else if (!result.equals(other.result))
+			return false;
+		return true;
+	}
 	
+	// toString
+	@Override
+	public String toString() {
+		return "ResultItem [result=" + result + "]";
+	}	
 	
 }
