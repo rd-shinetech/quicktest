4920523e86e137d7492f7c9d126b983d6439d7a3
Kevin Santos <kevin.santos@tecnico.ulisboa.pt>
2016-07-19 19:17:55 +0100

    Activity DTO corrected

---
+++ 03-Development/dto/src/main/java/co/shinetech/dto/Activity.java
@@ -0,0 +1,97 @@
+package co.shinetech.dto;
+
+import java.time.LocalDateTime;
+
+public class Activity implements Domain{
+
+
+	private static final long serialVersionUID = 1L;
+	private long id;
+	private int name;
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
+	private String className;
+	
+	public int getName() {
+		return name;
+	}
+	public long getPk(){
+		return id;
+	}
+	public void setName(int name) {
+		this.name = name;
+	}
+	public LocalDateTime getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(LocalDateTime startTime) {
+		this.startTime = startTime;
+	}
+	public LocalDateTime getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(LocalDateTime endTime) {
+		this.endTime = endTime;
+	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((className == null) ? 0 : className.hashCode());
+		result = prime * result + ((endTime == null) ? 0 : endTime.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + name;
+		result = prime * result + ((startTime == null) ? 0 : startTime.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Activity other = (Activity) obj;
+		if (className == null) {
+			if (other.className != null)
+				return false;
+		} else if (!className.equals(other.className))
+			return false;
+		if (endTime == null) {
+			if (other.endTime != null)
+				return false;
+		} else if (!endTime.equals(other.endTime))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name != other.name)
+			return false;
+		if (startTime == null) {
+			if (other.startTime != null)
+				return false;
+		} else if (!startTime.equals(other.startTime))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Activity [id=" + id + ", name=" + name + ", startTime=" + startTime + ", endTime=" + endTime
+				+ ", className=" + className + "]";
+	}
+	
+	
+}
