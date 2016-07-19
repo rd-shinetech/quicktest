fdd9c68ec381fa06b411aca94640d1c77c0772ea
Kevin Santos <kevin.santos@tecnico.ulisboa.pt>
2016-07-19 20:31:51 +0100

    ProfileTest added

--- 03-Development/dto/src/main/java/co/shinetech/dto/Activity.java
+++ 03-Development/dto/src/main/java/co/shinetech/dto/Activity.java
@@ -7,13 +7,13 @@ public class Activity implements Domain{
 
 	private static final long serialVersionUID = 1L;
 	private long id;
-	private int name;
+	private String name;
 	private LocalDateTime startTime;
 	private LocalDateTime endTime;
 	private String className;
 	private User teacher;
 	
-	public Activity(long id, int name, LocalDateTime startTime, LocalDateTime endTime, String className, User teacher) {
+	public Activity(long id, String name, LocalDateTime startTime, LocalDateTime endTime, String className, User teacher) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -31,11 +31,11 @@ public class Activity implements Domain{
 		this.id = id;
 	}
 	
-	public int getName() {
+	public String getName() {
 		return name;
 	}
 	
-	public void setName(int name) {
+	public void setName(String name) {
 		this.name = name;
 	}
 	public LocalDateTime getStartTime() {
@@ -66,13 +66,20 @@ public class Activity implements Domain{
 	public void setTeacher(User teacher) {
 		this.teacher = teacher;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((className == null) ? 0 : className.hashCode());
+		result = prime * result + ((endTime == null) ? 0 : endTime.hashCode());
 		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((startTime == null) ? 0 : startTime.hashCode());
+		result = prime * result + ((teacher == null) ? 0 : teacher.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -94,7 +101,10 @@ public class Activity implements Domain{
 			return false;
 		if (id != other.id)
 			return false;
-		if (name != other.name)
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
 			return false;
 		if (startTime == null) {
 			if (other.startTime != null)
@@ -108,6 +118,7 @@ public class Activity implements Domain{
 			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
 		return "Activity [id=" + id + ", name=" + name + ", startTime=" + startTime + ", endTime=" + endTime
