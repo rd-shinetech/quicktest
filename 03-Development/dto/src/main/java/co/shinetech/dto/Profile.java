de6a4760fa85c592f405f2010066ad4fc0e9726f
Gonxaluh <goncalochincho@gmail.com>
2016-07-19 20:06:20 +0100

    New test class ActivityTest, change to Profile's hash code

--- 03-Development/dto/src/main/java/co/shinetech/dto/Profile.java
+++ 03-Development/dto/src/main/java/co/shinetech/dto/Profile.java
@@ -34,11 +34,7 @@ public class Profile implements Domain{
 	
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
+		return (int) id;
 	}
 
 	@Override
