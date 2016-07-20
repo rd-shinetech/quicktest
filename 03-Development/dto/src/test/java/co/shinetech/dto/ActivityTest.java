8a6f32c6243f660a028a523b6d5b791916d3f307
Kevin Santos <kevin.santos@tecnico.ulisboa.pt>
2016-07-20 20:45:36 +0100

    ActivityTest fix

--- 03-Development/dto/src/test/java/co/shinetech/dto/ActivityTest.java
+++ 03-Development/dto/src/test/java/co/shinetech/dto/ActivityTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 /**
  * @author Goncalo
@@ -24,7 +25,9 @@ public class ActivityTest {
 		Profile prof = new Profile(400,"Teacher");
 		User teacher = new User(300,prof);
 		LocalDateTime ldt = LocalDateTime.now().plusHours(1);
-		Activity a = new Activity(39,"Joel",LocalDateTime.now(),ldt,"class1",teacher);
+		ActivityType type = ActivityType.EXERCISE;
+		Group group = new Group(10L,"turminha",new ArrayList<User>());
+		Activity a = new Activity(39,"Joel",LocalDateTime.now(),ldt,teacher,type,group);
 		assertNotNull(a);
 	}
 
@@ -34,8 +37,10 @@ public class ActivityTest {
 		User teacher = new User(300,prof);
 		LocalDateTime ldt = LocalDateTime.now().plusHours(1);
 		LocalDateTime ldtnow = LocalDateTime.now();
-		Activity a1 = new Activity(39,"Joel",ldtnow,ldt,"class1",teacher);
-		Activity a2 = new Activity(39,"Joel",ldtnow,ldt,"class1",teacher);
+		ActivityType type = ActivityType.EXERCISE;
+		Group group = new Group(10L,"turminha",new ArrayList<User>());
+		Activity a1 = new Activity(39,"Joel",ldtnow,ldt,teacher,type,group);
+		Activity a2 = new Activity(39,"Joel",ldtnow,ldt,teacher,type,group);
 		System.out.println(a1.toString());
 		System.out.println(a2.toString());
 		assertEquals(a1.hashCode(),a2.hashCode());	
