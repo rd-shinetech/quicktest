0f5be7cec4e33559842d75b24772632941893be0
ricardopnc <ric_ponc@sapo.pt>
2016-07-21 21:07:24 +0100

    Updates.

--- 03-Development/dao/src/test/java/co/shinetech/dao/ActivityDAOTest.java
+++ 03-Development/dao/src/test/java/co/shinetech/dao/ActivityDAOTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import co.shinetech.dao.db.PersistenceException;
 import co.shinetech.dao.factory.SerializerDBProviderFactory;
+import co.shinetech.dao.impl.ActivityDAOImp;
 import co.shinetech.dto.Activity;
 import co.shinetech.dto.ActivityType;
 import co.shinetech.dto.Group;
@@ -29,7 +30,7 @@ public class ActivityDAOTest {
 	User teacher = new User(54218547L, "Manuel", password, new Profile(839483948L, "name"));
 	ActivityType at = ActivityType.EXAME;
 	ArrayList<User> users = new ArrayList<>();	
-	Group group = new Group(25256543565L, "Maria Josï¿½", users);
+	Group group = new Group(25256543565L, "Maria José", users);
 	@SuppressWarnings("unchecked")
 	private static final PersistenceProvider<Activity> activityDao = SerializerDBProviderFactory
 	.getPersistenceProvider(SerializerDBProviderFactory.TABLE_ACTIVITY);
@@ -39,14 +40,22 @@ public class ActivityDAOTest {
 		// adding User to ArrayList, in this case, teacher
 		users.add(teacher);
 
-		Activity d = new Activity(id, name, startTime, endTime, teacher, at, group);
+		Activity d = new Activity(activityDao.nextId(), name, startTime, endTime, teacher, at, group);
 		activityDao.create(d);		
 		
 	}
 	@Test
 	public void testUpdateActivity() throws PersistenceException {
 		Activity d = new Activity(id, name, startTime, endTime, teacher, at, group);
+		if( activityDao.retrieveByID(id) == null ) {
+			activityDao.create(d);
+		}
+		d.setName("My activity");
 		activityDao.update(d);
 	}
 	
+	@Test
+	public void testRetrieveAll() throws PersistenceException{
+		activityDao.retrieveAll().forEach(i -> System.out.println(i));
+	}
 }
