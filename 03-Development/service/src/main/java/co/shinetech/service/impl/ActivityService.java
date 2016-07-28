4ee4b7fd40705f662aa4dae9543dba6599e67214
ricardopnc <ric_ponc@sapo.pt>
2016-07-28 21:37:32 +0100

    ...

---
+++ 03-Development/service/src/main/java/co/shinetech/service/impl/ActivityService.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package co.shinetech.service.impl;
+
+import java.util.List;
+
+import co.shinetech.dao.db.PersistenceException;
+import co.shinetech.dto.Activity;
+import co.shinetech.service.ServiceProvider;
+
+/**
+ * @author Ricardo
+ *
+ */
+public class ActivityService implements ServiceProvider<Activity> {
+
+	public void create(Activity d) throws PersistenceException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(Activity d) throws PersistenceException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Activity retrieveByID(int ID) throws PersistenceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Activity> retrieveAll() throws PersistenceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void delete(int ID) throws PersistenceException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int count() throws PersistenceException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public long nextId() throws PersistenceException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
