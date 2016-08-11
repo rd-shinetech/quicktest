6289b004d170e3f41401c6247d3b097696b53933
usuario <usuario@THINKPAD-SHINE.lan>
2016-08-11 21:45:10 +0100

    Update.

--- 03-Development/dto/src/main/java/co/shinetech/dto/ActivityArea.java
+++ 03-Development/dto/src/main/java/co/shinetech/dto/ActivityArea.java
@@ -8,19 +8,46 @@ package co.shinetech.dto;
  * @author Ricardo
  * @since 2016-08
  */
-public enum ActivityArea {
-	JAVAOCA("Java OCA"), JAVAOCP("Java OCP"), LINUX("Linux");
-	private String area;
+public class ActivityArea implements Domain {
+	private long id;
+	private String name;
 
-	private ActivityArea(String area) {
-		this.area = area;
+	private ActivityArea(long id) {
+		this.id = id;
 	}
 	
-	public String getArea() {
-		return area;
+	public long getPk() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
 	}
 
-	public void setArea(String area) {
-		this.area = area;
+	public void setName(String name) {
+		this.name = name;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ActivityArea other = (ActivityArea) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
 }
