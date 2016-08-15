6ed143ad226728ababc2473d6a0558da13aab6c4
ricardopnc <ric_ponc@sapo.pt>
2016-08-15 10:54:03 +0100

    ActivityAreaDAOTest started and incomplete.

---
+++ 03-Development/dao/src/test/java/co/shinetech/dao/ActivityAreaDAOTest.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package co.shinetech.dao;
+
+/**
+ * @author Ricardo
+ * @since 2016-08
+ *
+ */
+public class ActivityAreaDAOTest {
+	private static long id;
+	private static String name;
+	
+
+}
