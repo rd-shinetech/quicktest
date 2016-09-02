a795bb0191780aa11b0ca2b54ded88435e523271
ricardopnc <ric_ponc@sapo.pt>
2016-09-02 12:13:28 +0100

    Updates. Incomplete.

--- 03-Development/gui/src/main/java/co/shinetech/gui/question/QuestionDataPanel.java
+++ 03-Development/gui/src/main/java/co/shinetech/gui/question/QuestionDataPanel.java
@@ -3,13 +3,165 @@
  */
 package co.shinetech.gui.question;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import co.shinetech.dao.db.PersistenceException;
+import co.shinetech.dto.ActivityArea;
+import co.shinetech.dto.Question;
+import co.shinetech.gui.FilterPanel;
+import co.shinetech.gui.GUIUtils;
+import co.shinetech.gui.QTestMainWindow;
+import co.shinetech.gui.activityarea.ActivityAreaFormPanel;
+import co.shinetech.gui.table.DynamicTableModel;
 import co.shinetech.gui.table.GridDataPanel;
+import co.shinetech.service.ServiceFactory;
+import co.shinetech.service.impl.QuestionService;
 
 /**
  * @author Ricardo
  * @since 2016-08
  */
-/* public class QuestionDataPanel extends GridDataPanel {
+public class QuestionDataPanel extends GridDataPanel {
 	private JPanel mySelf;
-	private QuestionService qs = 
-} */
+	private QuestionService qs = ServiceFactory.getService(QuestionService.class);
+	
+	public QuestionDataPanel(DynamicTableModel tm) {
+		super(tm, "Catálogo de Questões");
+		mySelf = this;
+		loadData();
+	}
+	
+	private void loadData() {
+		Thread t = new Thread(
+				new Runnable() {
+				@Override
+				public void run() {
+					try {
+						QTestMainWindow.processStart();
+						Thread.sleep(1000L);
+						tableModel.setData(qs.retrieveAll());
+						tableModel.fireTableDataChanged();
+						table.repaint();
+						QTestMainWindow.processEnd();
+					} catch (PersistenceException e) {
+						JOptionPane.showMessageDialog(mySelf, "Erro a carregar dados da base de dados.", "Erro de Persistência", JOptionPane.ERROR_MESSAGE);
+					} catch (InterruptedException e) {
+					}
+				}
+		});
+		t.start();
+	}
+	
+	@Override
+	public ActionListener getCreateListener() {
+		return new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFrame f = (JFrame) SwingUtilities.getWindowAncestor(mySelf);
+				JDialog d = new JDialog(f,"Nova Questão");
+
+				d.setModal(true);
+				d.setResizable(false);
+				d.add(new ActivityAreaFormPanel(d));
+				d.pack(); // redimension the JDialog to the JPanel size
+				GUIUtils.centerOnParent(d, true);
+				d.setVisible(true);
+				loadData();
+			}
+		};
+	}
+	@Override
+	public ActionListener getRetrieveListener() {
+		return new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFrame f = (JFrame) SwingUtilities.getWindowAncestor(mySelf);
+				JDialog d = new JDialog(f,"Pesquisar Questão");
+				FilterPanel fp = new FilterPanel(d);
+				DefaultComboBoxModel<String> cbm = new DefaultComboBoxModel<>();
+				
+				cbm.addElement("ID");
+				cbm.addElement("Questão");
+				cbm.addElement("Resposta");
+				fp.setFieldComboBoxModel(cbm);
+				d.setModal(true);
+				d.setResizable(false);
+				d.add(fp);
+				d.pack(); // redimension the JDialog to the JPanel size
+				GUIUtils.centerOnParent(d, true);
+				d.setVisible(true);
+				
+				if( ! fp.isCancelled() ) {
+					List<Question> list;
+					
+					if( fp.getFieldComboBox().getSelectedItem().equals("ID") ) {
+						list = (List<Question>) tableModel.getData().stream().filter(o -> ((Question) o).getPk() == Long.valueOf(fp.getTipTextField().getText())).collect(Collectors.toList());
+					} else {
+						list = (List<Question>) tableModel.getData().stream().filter(o -> ((Question) o).getQuestion().contains(fp.getTipTextField().getText())).collect(Collectors.toList());
+					}					
+					tableModel.setData(list);
+					tableModel.fireTableDataChanged();
+					table.repaint();						
+				}
+			}
+		};
+		
+		@Override
+		public ActionListener getUpdateListener() {
+			return new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					JFrame f = (JFrame) SwingUtilities.getWindowAncestor(mySelf);
+					JDialog d = new JDialog(f,"Editar Questão");
+					QuestionFormPanel qfp;
+
+					d.setModal(true);
+					d.setResizable(false);
+					d.add(qfp = new QuestionFormPanel(d));
+					d.pack(); // redimension the JDialog to the JPanel size				
+					Question q = (Question) tableModel.getData().get(table.getSelectedRow());				
+					qfp.setDomainModel(q);
+					GUIUtils.centerOnParent(d, true);
+					d.setVisible(true);
+					loadData();
+				}
+			};		
+		}
+	}
+	@Override
+	public ActionListener getDeleteListener() {
+		return e -> {
+			if (table.getSelectedRow() < 0) {
+				JOptionPane.showMessageDialog(mySelf, "Por favor, Selecione uma Questão","Aviso",JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+			Question p = (Question) tableModel.getData().get(table.getSelectedRow());
+			try {
+				int i = JOptionPane.showConfirmDialog(mySelf, "Quer mesmo apagar a questão selecionada?", "Confirmação", JOptionPane.YES_NO_OPTION);
+				if (i == JOptionPane.YES_OPTION) {
+					qs.delete((int)p.getPk());
+					loadData();
+				}
+			}catch (PersistenceException e1) {
+				JOptionPane.showMessageDialog(mySelf, "Erro a remover dados da base de dados.", "Erro de Persistência", JOptionPane.ERROR_MESSAGE);
+			}
+		};
+	}
+
+	@Override
+	public ActionListener getReloadListener() {
+		return e -> {
+			loadData();
+		};
+	}
+} 
--- 03-Development/gui/src/main/java/co/shinetech/gui/question/QuestionFormPanel.java
+++ 03-Development/gui/src/main/java/co/shinetech/gui/question/QuestionFormPanel.java
@@ -6,6 +6,7 @@ package co.shinetech.gui.question;
 import javax.swing.JPanel;
 
 /**
+ * Form gui for question.
  * @author Ricardo
  * @since 2016-08
  */
